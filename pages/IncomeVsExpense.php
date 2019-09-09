1490c221ad8cc4a0b4b68e9d911afbc456fdf451
Takow Carvin <takowprogrammer22@gmail.com>
2019-09-09 08:38:01 +0100

    Project Commit

---
+++ pages/IncomeVsExpense.php
@@ -0,0 +1,23 @@
+<?php
+
+//Include Functions
+include('includes/Functions.php');
+
+//Include Notifications
+include ('includes/notification.php');
+
+
+
+
+//Include Global page
+	include ('includes/global.php');
+
+?>
+<div style="margin-left:250px;margin-top: -45px;">
+
+       <img src="./images/incomevsexpence.png" >
+       <h1>
+       	School Project
+       </h1>
+       <p>UB Budget Management System</p>
+   </div>
